67613840bc7de60ddd960ae2907638b9c6f221f9
Mihael Nikic <mnikic777@gmail.com>
2017-12-09 17:35:02 +0100

    added c-lion project template

---
+++ cmake-build-debug/CMakeFiles/bioinformatics.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/mihael/Documents/9. semestar/BIOINF/bioinformatics/src/main.cpp" "/home/mihael/Documents/9. semestar/BIOINF/bioinformatics/cmake-build-debug/CMakeFiles/bioinformatics.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")

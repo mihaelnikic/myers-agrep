2aa2de08f2dcfa60f327023b737650fa1b3b5537
Mihael Nikic <mnikic777@gmail.com>
2018-01-16 18:51:58 +0100

    add test program that outputs time and memory consumption

--- tests/tester.sh
+++ tests/tester.sh
@@ -1,10 +1,43 @@
 #!/usr/bin/env bash
 
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-        /usr/bin/time
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-        # Mac OSX
-        gtime
+function get_size_and_time() {
+    pattern="$1";k="$2";file="$3"
+    if [[ "$OSTYPE" == "linux-gnu" ]]; then
+           # size="$(/usr/bin/time --format="%M" ./agrep "$pattern" "$k" "$file" 2>&1 >/dev/null)";
+            ts=$(date +%s%N);
+            size="$(/usr/bin/time --format="%M" ./agrep "$pattern" "$k" "$file" 2>&1 >/dev/null)";
+            tt=$(( ($(date +%s%N) - $ts)/1000000));
+            echo "$tt;$size"
+
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+            # Mac OSX
+            gtime -v
+            ts=$(date +%s%N);
+            size="$(gtime --format="%M" ./agrep "$pattern" "$k" "$file" 2>&1 >/dev/null)";
+            tt=$(( ($(date +%s%N) - $ts)/1000000));
+            echo "$tt;$size"
+    else
+        (>2 echo "OS not supported!")
+        exit 1
+    fi
+}
+if [ "$#" -ne 3 ]; then
+    (2> echo "Expected three arguments: ([pattern] [k] [samples folder]) or ([sample file] [k] [pattern])!")
+    exit 1
+fi
+pattern="$1";k="$2";file_or_folder="$3"
+if [[ -d "$file_or_folder" ]]; then
+    #for f in "$file_or_folder"/*.parsed; do
+    #    echo "folder $f" $'\n'
+    #    result=$(get_size_and_time "$pattern" "$k" "$f")
+    #    echo "$f;$result\n"
+    #done
+    find "$file_or_folder" -name "*.parsed" -print0 | while read -d $'\0' file
+    do
+        result=$(get_size_and_time "$pattern" "$k" "$file")
+        echo "$file;$result"
+    done
 else
-    (>2 echo "OS nije podrzan!")
+    result=$(get_size_and_time "$pattern" "$k" "$file_or_folder")
+    echo "$file_or_folder;$result"
 fi
